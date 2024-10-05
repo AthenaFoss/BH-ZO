7f152eb9c9a77745746840580cae669a79349b16
AvhiMaz <avhimazumder5@outlook.com>
2024-10-05 22:48:03 +0530

    render hack

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import router from "./routes/userRoute";
 import leaderBoardRouter from "./routes/scoreRoute";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 app.use(cors());
@@ -12,5 +13,28 @@ app.use(cookieParser());
 
 app.use("/api/users", router);
 app.use("/api", leaderBoardRouter);
+
+const url = "https://bh-zo.onrender.com";
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response: any) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error: any) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
 
 export default app;
