888cfde7f7310686993e83a54759fa720a446dfe
manish kumar <iammanish61@gmail.com>
2024-10-06 12:11:20 +0530

    build leaderboard & wasd movement

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -3,21 +3,23 @@ import {
   SHIP_HEIGHT,
   SHIP_WIDTH,
   LEVEL_BOUNDS,
-  RED_LINE
+  RED_LINE,
 } from "./constants";
 
 const openInNewTab = (url) => {
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   link.href = url;
-  link.target = '_blank';
-  link.rel = 'noopener noreferrer';
+  link.target = "_blank";
+  link.rel = "noopener noreferrer";
   link.click();
 };
 
 const isWithinMovementBoundaries = (x, y) => {
-  if ((RED_LINE[y] && RED_LINE[y].includes(x)) ? window.location.href = "/game.html" : "/game.html")
-
-
+  if (
+    RED_LINE[y] && RED_LINE[y].includes(x)
+      ? (window.location.href = "/game.html")
+      : "/game.html"
+  )
     return !LEVEL_BOUNDS[y] ? true : !LEVEL_BOUNDS[y].includes(x);
 };
 
@@ -26,23 +28,22 @@ export const movePlayer = (keys, player) => {
   const absPlayerX = player.x + SHIP_WIDTH / 2;
   const absPlayerY = player.y + SHIP_HEIGHT / 2 + 20;
 
-
   if (
-    keys.includes("ArrowUp") &&
+    (keys.includes("ArrowUp") || keys.includes("KeyW")) &&
     isWithinMovementBoundaries(absPlayerX, absPlayerY - PLAYER_SPEED)
   ) {
     playerMoved = true;
     player.y = player.y - PLAYER_SPEED;
   }
   if (
-    keys.includes("ArrowDown") &&
+    (keys.includes("ArrowDown") || keys.includes("KeyS")) &&
     isWithinMovementBoundaries(absPlayerX, absPlayerY + PLAYER_SPEED)
   ) {
     playerMoved = true;
     player.y = player.y + PLAYER_SPEED;
   }
   if (
-    keys.includes("ArrowLeft") &&
+    (keys.includes("ArrowLeft") || keys.includes("KeyA")) &&
     isWithinMovementBoundaries(absPlayerX - PLAYER_SPEED, absPlayerY)
   ) {
     playerMoved = true;
@@ -50,7 +51,7 @@ export const movePlayer = (keys, player) => {
     player.flipX = true;
   }
   if (
-    keys.includes("ArrowRight") &&
+    (keys.includes("ArrowRight") || keys.includes("KeyD")) &&
     isWithinMovementBoundaries(absPlayerX + PLAYER_SPEED, absPlayerY)
   ) {
     playerMoved = true;
@@ -61,7 +62,16 @@ export const movePlayer = (keys, player) => {
 };
 
 export const movementAnimation = (keys, player) => {
-  const runningKeys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+  const runningKeys = [
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "KeyW",
+    "KeyA",
+    "KeyS",
+    "KeyD",
+  ];
   if (
     keys.some((key) => runningKeys.includes(key)) &&
     !player.anims.isPlaying
